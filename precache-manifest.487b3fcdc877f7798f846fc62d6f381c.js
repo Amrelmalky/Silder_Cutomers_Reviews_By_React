c3221a5a837d631ca98af1a36569328d94ae9d10
Amrelmalky <69693889+Amrelmalky@users.noreply.github.com>
2022-12-07 23:06:24 +0200

    Updates

---
+++ precache-manifest.487b3fcdc877f7798f846fc62d6f381c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bcbd070a690e07f71e842b44e0c925",
+    "url": "/Silder_Cutomers_Reviews_By_React/index.html"
+  },
+  {
+    "revision": "df0ac136264573501b68",
+    "url": "/Silder_Cutomers_Reviews_By_React/static/css/main.43576144.chunk.css"
+  },
+  {
+    "revision": "621e53d3930b552e8c4e",
+    "url": "/Silder_Cutomers_Reviews_By_React/static/js/2.5c458157.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Silder_Cutomers_Reviews_By_React/static/js/2.5c458157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0ac136264573501b68",
+    "url": "/Silder_Cutomers_Reviews_By_React/static/js/main.78cd0988.chunk.js"
+  },
+  {
+    "revision": "873a72225d46e1fb1785",
+    "url": "/Silder_Cutomers_Reviews_By_React/static/js/runtime-main.95b14ff4.js"
+  }
+]);
